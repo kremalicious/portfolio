2d50dddd0bb58bf58e530993feeffa54210ddc30
Matthias Kretschmann <m@kretschmann.io>
2018-03-22 12:53:42 +0100

    move over projects data

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,12 +1,26 @@
-import React from 'react'
+import React, { Component } from 'react'
+import projects from '../../data/projects.json'
 
-const Home = () => (
-  <main className="screen screen--home">
-    <a href="https://kremalicious.com">Blog</a>
-    <a href="https://github.com/kremalicious">GitHub</a>
-    <a href="https://dribbble.com/kremalicious">Dribbble</a>
-    <a href="https://twitter.com/kremalicious">Twitter</a>
-  </main>
-)
+class Home extends Component {
+  render() {
+    return (
+      <main className="screen screen--home">
+
+        <aside className="social">
+          <a href="https://kremalicious.com">Blog</a>
+          <a href="https://github.com/kremalicious">GitHub</a>
+          <a href="https://dribbble.com/kremalicious">Dribbble</a>
+          <a href="https://twitter.com/kremalicious">Twitter</a>
+        </aside>
+
+        <ul>
+          {projects.map(project => (
+            <li key={project.slug}>{project.name}</li>
+          ))}
+        </ul>
+      </main>
+    )
+  }
+}
 
 export default Home
