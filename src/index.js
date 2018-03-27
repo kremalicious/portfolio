a7376f23fd47618c17eb47eed51bc9d79e4cd59b
Matthias Kretschmann <m@kretschmann.io>
2018-03-27 10:00:52 +0200

    add react-snap

--- src/index.js
+++ src/index.js
@@ -1,14 +1,16 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
+import { hydrate, render } from 'react-dom'
 import { BrowserRouter as Router } from 'react-router-dom'
 import './index.css'
 import App from './App'
 import registerServiceWorker from './registerServiceWorker'
 
-ReactDOM.render(
-  <Router>
-    <App />
-  </Router>,
-  document.getElementById('root')
-)
+const rootElement = document.getElementById('root')
+
+if (rootElement.hasChildNodes()) {
+  hydrate(<Router><App /></Router>, rootElement)
+} else {
+  render(<Router><App /></Router>, rootElement)
+}
+
 registerServiceWorker()
