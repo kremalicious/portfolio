3b4500173f2a4ba253df61c7c15c27c0df02ad30
Matthias Kretschmann <m@kretschmann.io>
2018-06-22 23:43:24 +0200

    import base css

--- gatsby-config.js
+++ gatsby-config.js
@@ -14,7 +14,7 @@ module.exports = {
     'gatsby-transformer-sharp',
     'gatsby-plugin-sharp',
     'gatsby-plugin-sitemap',
-    'gatsby-plugin-offline',
+    // 'gatsby-plugin-offline',
     {
       resolve: 'gatsby-transformer-json',
       options: {
