4709eee3d89d1c99c43f1c04b347b556851bd3c3
Matthias Kretschmann <m@kretschmann.io>
2019-01-04 18:49:39 +0100

    remove promises

--- gatsby-node.js
+++ gatsby-node.js
@@ -58,36 +58,29 @@ exports.onCreateNode = ({ node, actions }) => {
 //
 exports.createPages = async ({ actions, graphql }) => {
   const { createPage } = actions
+  const template = path.resolve('src/templates/Project.jsx')
 
-  return new Promise((resolve, reject) => {
-    const template = path.resolve('src/templates/Project.jsx')
-
-    resolve(
-      graphql(`
-        {
-          allProjectsYaml {
-            edges {
-              node {
-                slug
-              }
-            }
+  const result = await graphql(`
+    {
+      allProjectsYaml {
+        edges {
+          node {
+            slug
           }
         }
-      `).then(result => {
-        if (result.errors) reject(result.errors)
+      }
+    }
+  `)
 
-        result.data.allProjectsYaml.edges.forEach(({ node }) => {
-          const { slug } = node
+  if (result.errors) throw result.errors
 
-          createPage({
-            path: slug,
-            component: template,
-            context: { slug }
-          })
-        })
+  result.data.allProjectsYaml.edges.forEach(({ node }) => {
+    const { slug } = node
 
-        resolve()
-      })
-    )
+    createPage({
+      path: slug,
+      component: template,
+      context: { slug }
+    })
   })
 }
