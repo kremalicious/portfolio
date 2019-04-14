0666dba344ad5adeb5fa3a080ec5034bcd5cee7c
Matthias Kretschmann <m@kretschmann.io>
2019-04-14 19:09:08 +0200

    cleanup

--- src/components/atoms/LinkIcon.test.jsx
+++ src/components/atoms/LinkIcon.test.jsx
@@ -4,21 +4,30 @@ import { render } from 'react-testing-library'
 import LinkIcon from './LinkIcon'
 
 describe('LinkIcon', () => {
-  const link = {
-    title: 'my project',
-    type: 'website'
-  }
-
   it('renders correctly', () => {
-    const { container } = render(
-      <LinkIcon title={link.title} type={link.type} />
+    const { container, rerender } = render(
+      <LinkIcon title={'my project'} type={'website'} />
     )
+    expect(container.firstChild.nodeName).toBe('svg')
+
+    rerender(<LinkIcon type={'github'} />)
+    expect(container.firstChild.nodeName).toBe('svg')
+
+    rerender(<LinkIcon type={'dribbble'} />)
+    expect(container.firstChild.nodeName).toBe('svg')
+
+    rerender(<LinkIcon type={'info'} />)
+    expect(container.firstChild.nodeName).toBe('svg')
+
+    rerender(<LinkIcon type={'download'} />)
+    expect(container.firstChild.nodeName).toBe('svg')
+
+    rerender(<LinkIcon type={'styleguide'} />)
     expect(container.firstChild.nodeName).toBe('svg')
   })
 
   it('does not render with unknown type', () => {
-    const link = { type: 'whatever' }
-    const { container } = render(<LinkIcon type={link.type} />)
+    const { container } = render(<LinkIcon type={'whatever'} />)
     expect(container.firstChild).not.toBeInTheDocument()
   })
 })
