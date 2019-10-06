831a8129f23d69b9ea45b0b264fcfd15e2af4f5a
Matthias Kretschmann <m@kretschmann.io>
2019-10-06 16:58:15 +0200

    deploy tweaks

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -12,6 +12,15 @@ set -e;
 
 function s3sync {
   aws s3 sync ./public s3://"$1" \
+    --include "*" \
+    --exclude "*.html" \
+    --exclude "sw.js" \
+    --exclude "*page-data.json" \
+    --exclude "chunk-map.json" \
+    --exclude "sitemap.xml" \
+    --exclude ".iconstats.json" \
+    --exclude "humans.txt" \
+    --exclude "robots.txt" \
     --cache-control public,max-age=31536000,immutable \
     --delete \
     --acl public-read
@@ -20,6 +29,7 @@ function s3sync {
     --exclude "*" \
     --include "*.html" \
     --include "sw.js" \
+    --include "*page-data.json" \
     --include "chunk-map.json" \
     --include "sitemap.xml" \
     --include ".iconstats.json" \
