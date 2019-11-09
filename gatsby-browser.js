8bc26fe316eef35239348dac65575efa69300ed1
Matthias Kretschmann <m@kretschmann.io>
2019-11-09 20:09:06 +0100

    Display a message when a service worker updates

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -22,3 +22,15 @@ wrapRootElement.propTypes = {
 
 // Layout with Page Transitions
 export const wrapPageElement = wrapPageElementWithLayout
+
+// Display a message when a service worker updates
+// https://www.gatsbyjs.org/docs/add-offline-support-with-a-service-worker/#displaying-a-message-when-a-service-worker-updates
+export const onServiceWorkerUpdateReady = () => {
+  const answer = window.confirm(
+    `This application has been updated. ` +
+      `Reload to display the latest version?`
+  )
+  if (answer === true) {
+    window.location.reload()
+  }
+}
