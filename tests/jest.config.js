f98816ca8c71db078aa77c4f2db4b897f5bc30ff
Matthias Kretschmann <m@kretschmann.io>
2021-03-13 17:46:54 +0100

    test fixes

--- tests/jest.config.js
+++ tests/jest.config.js
@@ -1,5 +1,8 @@
 module.exports = {
   rootDir: '../',
+  transform: {
+    '^.+\\.[jt]sx?$': ['babel-jest', { configFile: './tests/babel.config.js' }]
+  },
   moduleNameMapper: {
     '.+\\.(css|styl|less|sass|scss)$': 'identity-obj-proxy',
     '.+\\.(jpg|jpeg|png|gif|eot|otf|webp|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
