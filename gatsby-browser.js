3f55d2dda89b1960060d125d6939ad29815a53e0
Matthias Kretschmann <m@kretschmann.io>
2021-09-19 16:22:05 +0200

    service worker update notification

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -2,6 +2,7 @@ import wrapPageElementWithLayout from './src/helpers/wrapPageElement'
 
 // Global styles
 import './src/styles/global.css'
+import './src/styles/_toast.css'
 
 // IntersectionObserver polyfill for gatsby-image (Safari, IE)
 if (typeof window !== 'undefined' && !window.IntersectionObserver) {
@@ -27,11 +28,9 @@ export const shouldUpdateScroll = ({
 // Display a message when a service worker updates
 // https://www.gatsbyjs.org/docs/add-offline-support-with-a-service-worker/#displaying-a-message-when-a-service-worker-updates
 export const onServiceWorkerUpdateReady = () => {
-  const answer = window.confirm(
-    'This application has been updated. ' +
-      'Reload to display the latest version?'
-  )
-  if (answer === true) {
-    window.location.reload()
-  }
+  const div = document.createElement('div')
+  div.id = 'toast'
+  div.classList.add('alert', 'alert-info')
+  div.innerHTML = `<button onClick="window.location.reload()">Updates are available. <span>Click to Reload</span>.</button>`
+  document.body.append(div)
 }
