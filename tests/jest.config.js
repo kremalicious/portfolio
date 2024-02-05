fbd2094ec35a0f063c3ce8612248da9d903b2e1e
Matthias Kretschmann <m@kretschmann.io>
2024-02-05 23:36:45 +0000

    fix jest config

--- tests/jest.config.js
+++ tests/jest.config.js
@@ -1,17 +1,13 @@
-import nextJest from 'next/jest'
-import type { Config } from 'jest'
+import nextJest from 'next/jest.js'
 
 const createJestConfig = nextJest({
-  // Provide the path to your Next.js app to load next.config.js and .env files in your test environment
   dir: './'
 })
 
-// Add any custom config to be passed to Jest
-const customJestConfig: Config = {
-  rootDir: '../', // = /
-  // Add more setup options before each test is run
+/** @type {import('jest').Config} */
+const customJestConfig = {
+  rootDir: '../',
   setupFilesAfterEnv: ['<rootDir>/tests/jest.setup.tsx'],
-  // if using TypeScript with a baseUrl set to the root directory then you need the below for alias' to work
   moduleDirectories: ['node_modules', '<rootDir>/src'],
   testEnvironment: 'jsdom',
   moduleNameMapper: {
