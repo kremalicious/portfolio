43e990626f51d098e0db0f26d12dacd61574ef8d
Matthias Kretschmann <m@kretschmann.io>
2025-11-01 15:44:51 +0000

    fixes

--- src/features/not-found/api/get-random-gif.ts
+++ src/features/not-found/api/get-random-gif.ts
@@ -2,6 +2,8 @@ import { GiphyFetch } from '@giphy/js-fetch-api'
 
 export async function getRandomGif(tag: string) {
   try {
+    // Famous last words:
+    // "It's just the 404 page so why not expose the dev API key"
     const giphyClient = new GiphyFetch(
       import.meta.env.PUBLIC_GIPHY_API_KEY || ''
     )
